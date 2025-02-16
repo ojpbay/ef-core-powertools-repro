f810f7d9373cbb1319086d691de3d927ac176823
ojpbay <oli.baylis@live.co.uk>
2025-02-16 10:47:38 +0000

    Empty contracts project before reverse engineering from sample db project

---
+++ EFCorePowerToolsRepro/Database/Database/MyEntity.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MyEntity]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [SomeDeltaId] INT NULL, 
+    [StatusId] INT NULL, 
+    CONSTRAINT [FK_MyEntity_SomeDelta_SomeDelta_Id] FOREIGN KEY ([SomeDeltaId]) REFERENCES [dbo].[SomeDelta]([Id]),
+    CONSTRAINT [FK_MyEntity_StatusId_Status_Id] FOREIGN KEY ([StatusId]) REFERENCES [dbo].[Status]([Id])
+)
